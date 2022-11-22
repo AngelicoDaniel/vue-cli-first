eb19a26a0cc3817b21bfc9de30ee3ba975f3c295
Daniel <angelicodaniel93@gmail.com>
2022-11-22 16:28:45 +0100

    last commit

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <!--nostro codice-->
+        <h2>La mia prima app con Vue CLI!</h2>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'AppTitle',
+}
+</script>
